91db2f6b380edc3c122b9d3937cf3806e1dcc122
Paul Maliakel <paulbalu93@gmail.com>
2021-09-12 02:12:47 +0200

    adds Features

---
+++ shortly/components/Features.js
@@ -0,0 +1,15 @@
+function Features() {
+  return (
+    <div className="mt-20">
+      <h1> Features</h1>
+
+      <div className="bg-gray-100 mt-20">
+        <div className="text-6xl font-poppins font-bold flex align-items justify-center pt-20 mt-20">
+          <h1> Advanced Statistics </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
