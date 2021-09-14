36b02433bf8c820a8a3604216e00c9ffb0038d8e
Paul Maliakel <paulbalu93@gmail.com>
2021-09-14 17:19:08 +0200

    adds feature details section

--- shortly/components/Features.js
+++ shortly/components/Features.js
@@ -1,14 +1,15 @@
+import Image from "next/image";
 function Features() {
   return (
     <div className="mt-40 relative">
       <div className="p-2"></div>
 
-      <div className="bg-gray-100 mt-20 pt-20">
+      <div className="bg-gray-200 mt-20 pt-20">
         <div className=" pt-20 mt-20 ">
           <div className="text-6xl  font-bold flex align-items justify-center  ">
             <h1> Advanced Statistics </h1>
           </div>
-          <div className=" flex align-items justify-center font-poppins text-gray-500 mt-8 text-2xl">
+          <div className=" flex align-items justify-center font-poppins text-gray-500 mt-8 text-2xl mb-28">
             <div>
               <p>
                 Track how your links are performing across the web with <br />
@@ -20,41 +21,31 @@ function Features() {
             </div>
           </div>
         </div>
-        <div className="ml-20">
-          <div className="grid grid-cols-3 ml-20 ">
-            <div className="bg-white ">
-              <h1 className="text-3xl font-bold font-poppins">
-                Brand Recognition
-              </h1>
-              <div>
-                <p> Boost your brand recognition with</p>
-                <p> each click. Generic links don't mean a </p>
-                <p> thing. Branded links help instil</p>
-                <p> confidence in your content</p>
+        {/* Feature details section */}
+        <div className="pb-10 mx-28">
+          {/* container of 3 feature columns */}
+          <div className="flex align-items ml-10">
+            {/* first column */}
+            <div className="bg-white shadow-sm rounded-lg relative w-[398px] h-[304px] ">
+              <div className="absolute -top-14 rounded-full bg-dark_violet-light h-24 w-24 ">
+                <div className="p-7">
+                  <Image
+                    src="/icon-brand-recognition.svg"
+                    width="40"
+                    height="40"
+                    objectFit="contain"
+                  />
+                </div>
               </div>
             </div>
-            <div>
-              <h1> Detailed Records</h1>
-              <div>
-                <p> Gain insights into who is clicking your</p>
-                <p>links. Knowing when and where</p>
-                <p> people engage with your content</p>
-                <p> helps inform better decisions</p>
-              </div>
-            </div>
-            <div>
-              <h1> Fully Customizable</h1>
-              <div>
-                <p> Improve brand awareness and</p>
-                <p>content discoverability through</p>
-                <p>customizable lnks, supercharging</p>
-                <p>audience engagement</p>
-              </div>
-            </div>
+            {/* second column */}
+            <div className="bg-white shadow-sm rounded-lg relative w-[398px] h-[304px] "></div>
+            {/* third column */}
+            <div className="bg-white shadow-sm rounded-lg relative w-[398px] h-[304px] "></div>
           </div>
         </div>
       </div>
-      <div className="absolute top-0 left-60  right-60 bg-shorten-desktop  rounded-lg bg-center bg-cover bg-dark_violet-light  z-50 ">
+      <div className="absolute top-0 left-36  right-36 bg-shorten-desktop  rounded-lg bg-center bg-cover bg-dark_violet-light  z-50 ">
         <div className="flex items-center justify-center p-6 pl-12  py-10 ">
           {/* input box */}
           <div className="flex items-center bg-white rounded-lg py-4 px-12 md:shadow-sm m-5 p flex-grow hover:ring-2 hover:ring-red-500 ">
