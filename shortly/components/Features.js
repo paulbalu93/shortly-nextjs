c5a0b1a1a3c7f4b094d0cc862c7f603f7536580a
Paul Maliakel <paulbalu93@gmail.com>
2021-09-22 10:50:55 +0200

    adds mobile optimisation to features component

--- shortly/components/Features.js
+++ shortly/components/Features.js
@@ -38,15 +38,15 @@ function Features() {
   // // );
 
   return (
-    <div className="mt-40 relative">
-      <div className="p-2"></div>
+    <div className="lg:mt-40 relative">
+      <div className="lg:p-2"></div>
 
       <div className="bg-gray-200 mt-20 pt-20">
         <div className=" pt-20 mt-20 ">
-          <div className="text-6xl  font-bold flex align-items justify-center  ">
+          <div className="hidden lg:flex lg:text-6xl text-4xl  font-bold align-items justify-center  ">
             <h1> Advanced Statistics </h1>
           </div>
-          <div className=" flex align-items justify-center font-poppins text-gray-500 mt-8 text-2xl mb-28">
+          <div className=" hidden lg:flex align-items justify-center font-poppins text-gray-500 mt-8 lg:text-2xl mb-28">
             <div>
               <p>
                 Track how your links are performing across the web with <br />
@@ -57,14 +57,29 @@ function Features() {
               </div>
             </div>
           </div>
+          <div className="lg:hidden text-4xl  font-bold flex align-items justify-center  ">
+            <h1> Advanced Statistics </h1>
+          </div>
+          <div className="lg:hidden flex align-items justify-center font-poppins text-gray-500 mt-8 lg:text-2xl mb-28">
+            <div>
+              <p>Track how your links are performing</p>
+
+              <div className="justify-center flex mt-1">
+                <p>across the web with our advanced </p>
+              </div>
+              <div className=" justify-center flex mt-1">
+                <p>statistics dashboard </p>
+              </div>
+            </div>
+          </div>
         </div>
         {/* Feature details section */}
-        <div className="pb-36 mx-64">
+        <div className="pb-36 lg:mx-64">
           {/* container of 3 feature columns */}
-          <div className="flex align-items ml-10">
+          <div className="md:flex align-items lg:ml-10 mx-6 ">
             {/* first column */}
-            <div className="bg-white shadow-sm rounded-lg relative w-[398px] h-[304px] ">
-              <div className="absolute -top-14 left-8 rounded-full bg-dark_violet-light h-24 w-24 ">
+            <div className="bg-white shadow-sm rounded-lg relative md:w-[398px] md:h-[304px] pt-1  ">
+              <div className="absolute lg:-top-14 lg:left-8 -top-14 left-32 rounded-full bg-dark_violet-light h-24 w-24 ">
                 <div className="p-7">
                   <Image
                     src="/icon-brand-recognition.svg"
@@ -74,14 +89,25 @@ function Features() {
                   />
                 </div>
               </div>
-              <h1 className="text-2xl font-bold font-poppins mt-20 ml-8">
+              {/* desktop */}
+              <h1 className="hidden lg:inline-flex text-2xl font-bold font-poppins mt-10 ml-8 mr-8">
                 Brand Recognition
               </h1>
-              <div className="pt-6 text-lg font-poppins text-gray-500 ml-8">
+              <div className="hidden lg:inline-block pt-6 text-lg font-poppins  text-gray-500 ml-8">
                 <p> Boost your brand recognition with</p>
                 <p> each click. Generic links don't mean a </p>
                 <p> thing. Branded links help instil</p>
                 <p> confidence in your content</p>
+              </div>
+              {/* mobile */}
+              <h1 className="text-2xl text-center lg:hidden font-bold font-poppins mt-16 mx-4">
+                Brand Recognition
+              </h1>
+              <div className="pt-6 lg:hidden text-lg font-poppins text-center text-gray-500 mx-4">
+                <p> Boost your brand recognition with</p>
+                <p> each click. Generic links don't </p>
+                <p> mean a thing. Branded links help </p>
+                <p> instil confidence in your content</p>
               </div>
             </div>
             {/* line */}
